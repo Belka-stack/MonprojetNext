09d5c9c50fb9e82a40595ed54c7e2ac62e9b94f9
Belka-stack <Belkabakouche@gmail.com>
2024-10-17 17:21:35 +0200

    First commit 17 octobre 2024

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,2 @@
-
 import './styles/app.css';
 import './js/menuBurger.js'
-
-
-
--- assets/js/menuBurger.js
+++ assets/js/menuBurger.js
@@ -1,18 +1,19 @@
 // Menu Burger
 
-var navmob = document.getElementById("mobile");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
+var navmob = document.getElementById("mobile"); // Sélectionne l'élément du menu mobile en utilisant son ID mobile.
+var openBtn = document.getElementById("openBtn"); // Sélectionne le bouton qui ouvre le menu en utilisant son ID openBtn.
+var closeBtn = document.getElementById("closeBtn");// Sélectionne le bouton qui ferme le menu en utilisant son ID closeBtn.
 
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
+openBtn.onclick = openNav;// Lorsque l'utilisateur clique sur le bouton d'ouverture, la fonction openNav est appelée.
+closeBtn.onclick = closeNav;// Lorsque l'utilisateur clique sur le bouton de fermeture, la fonction closeNav est appelée.
 
-/* Set the width of the side navigation to 250px */
+
 function openNav() {
-navmob.classList.add("active");
+navmob.classList.add("active"); // Cette fonction ajoute la classe active à l'élément navmob, ce qui active le menu.
 }
 
-/* Set the width of the side navigation to 0 */
+
 function closeNav() {
-navmob.classList.remove("active");
+navmob.classList.remove("active");// Cette fonction supprime la classe active de l'élément navmob, fermant ainsi le menu.
 }
+
