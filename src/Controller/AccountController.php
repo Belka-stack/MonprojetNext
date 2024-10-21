04fa906a01c00d2a33856a038bd1f13acf258679
Belka-stack <Belkabakouche@gmail.com>
2024-10-22 01:56:01 +0200

    dernier commit

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Form\PasswordUserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AccountController extends AbstractController
 {
@@ -16,17 +18,24 @@ class AccountController extends AbstractController
         return $this->render('account/index.html.twig');
     }
 
-    #[Route('/account/modify-pwd', name: 'app_account_mofify_pwd')]
-    public function password(Request $request): Response
+    #[Route('/account/modify-pwd', name: 'app_account_modify_pwd')]
+
+    // On utilise les injection de dépendances UserPasswordHasherInterface $passwordHasher pour hascher et verifier le mot de passe et on aimerait les passer à notre formulaire.
+
+    public function password(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
 
-        $form = $this->createForm(PasswordUserType::class,$user);
+        // En troisième paramètre de ma méthode createForm,j'ajoute un tableau et dans ce tableau nous pouvons envoyer des infos à notre formulaire.
+
+        $form = $this->createForm(PasswordUserType::class,$user, [
+            'passwordHasher' => $passwordHasher
+        ]);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            dd($form->getData()); 
+            $entityManager->flush();// Pour mettre à jour BDD 
         }
 
         return $this->render('account/password.html.twig', [
--- src/Form/PasswordUserType.php
+++ src/Form/PasswordUserType.php
@@ -10,6 +10,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 
@@ -59,7 +60,31 @@ class PasswordUserType extends AbstractType
         ])
 
         ->addEventListener(FormEvents::SUBMIT, function (FormEvent $event){  
-            // die('ok mon even marche');
+
+            // Récupération du formulaire plus data user
+
+            $form =$event->getForm(); // On va chercher notre formulaire
+            $user = $form->getConfig()->getOptions()['data']; // On va chercher notre user actuel
+            $passwordHasher = $form->getConfig()->getOptions()['passwordHasher']; // On est venu chercher ce qui nous permet de vérifier l'encodage d'un mot de passe
+            
+            //1.Récupérer le mot de passe saisi par l’utilisateur et le comparer au mdp en BDD.Il sera à true s'il est valid et false si ce n'est pas le cas.
+
+            $isValid = $passwordHasher->isPasswordValid(
+                $user,
+                $form->get('actualPassword')->getData()
+            );
+            
+    
+        
+
+        // 3. Si c’est != envoyer une erreur
+
+        if (!$isValid) {
+            // On va chercher notre formulaire,puis le champs qui est concerné par l'erreur,en l'occurence actual password et on lui la méthode addError() qui permet d'ajouter des erreurs à la volée à des input de notre formulaire.Et elle accepte en paramètre un nouvelle objet FormError qui délivre un message d'erreur si le mot de passe n'est pasconforme.
+
+            $form->get('actualPassword')->addError(new FormError("Your current password is not compliant. Please enter your old password."));
+        }
+            
         })
     
 
@@ -70,8 +95,10 @@ class PasswordUserType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+        // Par défault notre passwordHasher est null si le formaulaire n'a pas été envoyé dans les options 
         $resolver->setDefaults([
             'data_class' => User::class,
+            'passwordHasher' => null
         ]);
     }
 }
